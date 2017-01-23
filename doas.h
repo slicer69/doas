453102110aa59b7446ee2480a3fc8878a4f785a5
katakk <kata@katakuri.sakura.ne.jp>
2017-01-24 01:12:56 +0900

    Merge, update from OpenBSD

--- doas.h
+++ doas.h
@@ -1,4 +1,20 @@
-/* $OpenBSD: doas.h,v 1.8 2016/06/19 19:29:43 martijn Exp $ */
+/* $OpenBSD: doas.h,v 1.12 2016/10/05 17:40:25 tedu Exp $ */
+/*
+ * Copyright (c) 2015 Ted Unangst <tedu@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 struct rule {
 	int action;
 	int options;
@@ -10,10 +26,8 @@ struct rule {
 };
 
 extern struct rule **rules;
-extern int nrules, maxrules;
+extern int nrules;
 extern int parse_errors;
-
-size_t arraylen(const char **);
 
 char **prepenv(struct rule *);
 
@@ -22,6 +36,7 @@ char **prepenv(struct rule *);
 
 #define NOPASS		0x1
 #define KEEPENV		0x2
+#define PERSIST		0x4
 
 #ifndef UID_MAX
 #define UID_MAX 65535
